0afafeafc44992760f5e928e84853d5104ca3666
Knut Andreas Meyer <knutam@gmail.com>
2022-07-25 21:29:33 +0200

    Structured documentation

--- README.md
+++ README.md
@@ -4,3 +4,55 @@
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://KnutAM.github.io/FerriteNeumann.jl/dev)
 [![Build Status](https://github.com/KnutAM/FerriteNeumann.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/KnutAM/FerriteNeumann.jl/actions/workflows/CI.yml?query=branch%3Amain)
 [![Coverage](https://codecov.io/gh/KnutAM/FerriteNeumann.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/KnutAM/FerriteNeumann.jl)
+
+Simplified application of Neumann boundary conditions for [Ferrite.jl](https://github.com/Ferrite-FEM/Ferrite.jl/), 
+where the Neumann BCs are of the type
+
+- Scalar field: $\int_{\Gamma} b \ \delta u \ \mathrm{d}\Gamma$
+- Vector field: $\int_{\Gamma} \boldsymbol{b} \cdot \boldsymbol{\delta u} \ \mathrm{d}\Gamma$
+
+## Example
+Let's consider the case of a coupled problem with one 
+scalar, `:c`, and one vector, `:u`, field. 
+
+We start by defining how the Neumann boundary conditions should vary with 
+position (within the given face set), time, and normal vector. 
+Let's say we want to apply a pressure as a function of height (`x[3]`) on 
+the displacement field, `:u`, and a ramping diffusion flux for the 
+concentration field, `:c`. 
+```julia
+function f_u(x::Vec, time, n::Vec)
+    ρ = 1000; g = 9.81
+    return -ρ*g*x[3]*n              # Returns a Vec
+end
+f_c(x::Vec, time, n::Vec) = time    # Returns a Number
+```
+
+Note that for scalar fields, `fv::FaceScalarValues` and `f` should 
+return a scalar. For vector fields, `fv::FaceVectorValues` and `f` 
+should return a `Vec` with the same dimension as the field.
+
+With the time and spatial variation defined, we can setup the actual 
+boundary value types, starting with the container, defining the faceset, 
+the facevalues, and finally adding each condition
+
+```julia
+nh = NeumannHandler(dh::AbstractDofHandler) # Container for Neumann BCs
+faceset = getfaceset(grid, "right")         # 
+qr = QuadratureRule{dim-1, RefCube}(2)      # 
+ip = Lagrange{dim, RefCube, 1}()            #
+fv_c = FaceScalarValues(qr, ip)             # :c requires scalar values
+fv_u = FaceVectorValues(qr, ip)             # :u requires vector values
+add!(nh, Neumann(:c, fv_c, faceset, f_c))   #
+add!(nh, Neumann(:u, fv_u, faceset, f_u))   # 
+```
+
+During the time stepping, the current forces can be added to the force vector with 
+
+```julia
+apply!(f::Vector, nh, time)
+```
+noting that the full force vector is added (not only the increment), so this statement 
+is usually prepended by `fill!(f, 0)`. However, this action can also be done before 
+the element assembly if it includes `f`, nothing that start_assemble has this effect
+if used on `f`. 
--- src/neumann.jl
+++ src/neumann.jl
@@ -1,11 +1,31 @@
-# If face_value is ScalarValued, f should return scalar 
-# If face_value is VectorValued, f should return a vector
-struct Neumann{FV,FT}
+"""
+    Neumann(field_name::Symbol, fv::FaceValues, faceset::Set{FaceIndex}, f, cellset=nothing)
+
+    Define a Neumann contribution according to 
+    ```math
+    \\int_{\\Gamma} b \\ \\delta u \\ \\mathrm{d}\\Gamma
+
+    \\int_{\\Gamma} \\boldsymbol{b} \\cdot \\boldsymbol{\\delta u} \\ \\mathrm{d}\\Gamma
+    ```
+    for scalar or vector fields, respectively. Here ``b`` or 
+    ``\\boldsymbol{b}`` is the prescribed Neumann value, 
+    defined by the function `f` with signature 
+    ``` 
+    f(x::Vec, time, n::Vec)
+    ```
+    where `x` is the spatial position of the current quadrature point, `time` is the 
+    current time, and `n` is the face normal vector. `field_name` describes the 
+    field on which the boundary condition should act, and `fv` describes the 
+    interpolation and integration rules. `faceset` describes which faces the BC is 
+    applied to, and if `cellset!=nothing`, only the cells in `faceset` that are also 
+    in `cellset` are included (important when using mixed grids with different cells)
+"""
+struct Neumann{FV,FUN}
     field_name::Symbol
     face_values::FV
     faces::Vector{Int}
     cells::Vector{Int}
-    fun::FT # f(x::Vec, t::Number, normal::Vec)->Union{<:Number, <:Vec}
+    f::FUN # f(fv::FaceValues, q_point::Int, i_shape::Int, x::Vec, t::Number)->Number
 end
 
 function Neumann(field_name::Symbol, fv::FaceValues, faceset::Set{FaceIndex}, f, cellset=nothing)
@@ -14,61 +34,41 @@ function Neumann(field_name::Symbol, fv::FaceValues, faceset::Set{FaceIndex}, f,
 end
 
 struct NeumannHandler{DH<:AbstractDofHandler}
-    nbc::Vector{Neumann}
+    nbcs::Vector
     dh::DH
 end
 
-NeumannHandler(dh::AbstractDofHandler) = NeumannHandler(Neumann[], dh)
+NeumannHandler(dh::AbstractDofHandler) = NeumannHandler([], dh)
 
-function Ferrite.add!(nh::NeumannHandler, nbc::Neumann)
+function Ferrite.add!(nh::NeumannHandler, nbc)
     # Should make some checks here...
-    push!(nh.nbc, nbc)
+    push!(nh.nbcs, nbc)
 end
 
 function Ferrite.apply!(f::Vector, nh::NeumannHandler, time)
-    foreach(nbc->apply!(f,nbc,nh.dh,time), nh.nbc)
+    foreach(nbc->apply!(f,nbc,nh.dh,time), nh.nbcs)
 end
 
 function Ferrite.apply!(f::Vector{T}, nbc::Neumann, dh::DofHandler, time) where T
     dofs = collect(dof_range(dh, nbc.field_name))
     fe = zeros(T, length(dofs))
     for face in FaceIterator(dh, nbc.faces, nbc.cells)
-        calculate_neumann_contribution!(fe, face, nbc.face_values, time, nbc.fun)
+        calculate_neumann_contribution!(fe, face, nbc.face_values, time, nbc.f)
         assemble!(f, view(celldofs(face), dofs), fe)
     end
 end
 
-function calculate_neumann_contribution!(fe::Vector, face::FaceIterator, fv::FaceValues, time, fun)
+function calculate_neumann_contribution!(fe::Vector, face::FaceIterator, fv::FaceValues, time, f)
     fill!(fe, 0)
     reinit!(fv, face)
     for q_point in 1:getnquadpoints(fv)
         dΓ = getdetJdV(fv, q_point)
         x = spatial_coordinate(fv, q_point, getcoordinates(face))
         n = getnormal(fv, q_point)
-        b = fun(x, time, n)
+        b = f(x, time, n)
         for i in 1:getnbasefunctions(fv)
             δu = shape_value(fv, q_point, i)
-            fe[i] += δu ⋅ b * dΓ
+            fe[i] += (δu ⋅ b) * dΓ
         end
     end
 end
-
-#= Alternative method which in one way doesn't include the "weak form" per say, but
-# I'm not sure if it brings any advantages. =#
-function calculate_neumann_contribution2!(fe::Vector, face::FaceIterator, fv::FaceValues, time, fun)
-    fill!(fe, 0)
-    reinit!(fv, face)
-    for q_point in 1:getnquadpoints(fv)
-        x = spatial_coordinate(fv, q_point, getcoordinates(face))
-        for i in 1:getnbasefunctions(fv)
-            fe[i] += fun(fv, q_point, i, x, time)
-        end
-    end
-end
-
-function neumann_f_example(fv, q_point, i, x, time)
-    δu = shape_value(fv, q_point, i)
-    b = my_b_fun(x, time, getnormal(fv, q_point))
-    dΓ = getdetJdV(fv, q_point)
-    return (δu ⋅ b) * dΓ
-end 
